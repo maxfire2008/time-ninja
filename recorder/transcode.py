0a82593e9e8ca3072840468f8e3e937dbff75630
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2024-01-23 20:41:24 +1100

    rewrite concatonation code

--- recorder/transcode.py
+++ recorder/transcode.py
@@ -1,24 +1,39 @@
 import pathlib
+import subprocess
 
 
-def transcode(directory: pathlib.Path) -> None:
-    for item in sorted(directory.iterdir()):
-        if not item.exists():
-            continue
-        if item.is_dir():
-            transcode(item)
-        if item.suffix == ".mp4":
-            # convert the name to an integer
-            # then subtract 1 and convert back to a string with 0 padding to make it the same length
-            previous_item_name = (
-                str(int(item.stem) - 1).zfill(len(item.stem)) + item.suffix
-            )
+def concat(directory: pathlib.Path) -> bool:
+    next_tracker_file = directory / "next"
+    if not next_tracker_file.exists():
+        next_tracker = 0
+    else:
+        next_tracker = int(next_tracker_file.read_text())
 
-            if (directory / previous_item_name).exists():
-                # add the current item to the previous item
-                with open(directory / previous_item_name, "ab") as f:
-                    f.write(item.read_bytes())
-                # delete the current item
-                item.unlink()
-                # rename the previous item to the current item
-                (directory / previous_item_name).rename(item)
+    next_file = directory / f"{next_tracker:08d}.mp4"
+
+    if not next_file.exists():
+        return False
+
+    # append the next file to out.mp4
+    out_file = directory / "out.mp4"
+
+    if not out_file.exists():
+        out_file.touch()
+
+    with open(out_file, "ab") as out:
+        with open(next_file, "rb") as next:
+            out.write(next.read())
+
+    # delete the next file
+    next_file.unlink()
+
+    # increment the next tracker
+    next_tracker += 1
+    next_tracker_file.write_text(str(next_tracker))
+
+    return True
+
+
+def concat_all(directory: pathlib.Path) -> None:
+    while concat(directory):
+        pass
