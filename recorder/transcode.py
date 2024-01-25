995eb88efebd8b8a6ecb4e0dd9247df90ad369f5
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2024-01-25 21:29:12 +1100

    attempt to write a functional transcoder

--- recorder/transcode.py
+++ recorder/transcode.py
@@ -3,49 +3,45 @@ import subprocess
 import random
 
 
-def concat(directory: pathlib.Path) -> bool:
-    lock_file = directory / "lock"
-    if lock_file.exists():
-        return True
+def transcode(file_path: pathlib.Path) -> None:
+    transcode_output = file_path.parent / f"{file_path.stem}.ts"
+    if transcode_output.exists():
+        return
 
-    lock_bytes = random.randbytes(128)
-    lock_file.write_bytes(lock_bytes)
-    if lock_file.read_bytes() != lock_bytes:
-        return True
+    M3U8 = f"""#EXTM3U
+#EXT-X-VERSION:7
+#EXT-X-TARGETDURATION:{15*(int(file_path.stem)+1)}
+#EXT-X-MEDIA-SEQUENCE:0
+#EXT-X-PLAYLIST-TYPE:VOD
+"""
 
-    next_tracker_file = directory / "next"
-    if not next_tracker_file.exists():
-        next_tracker = 0
-    else:
-        next_tracker = int(next_tracker_file.read_text())
+    for i in range(int(file_path.stem) + 1):
+        if not (file_path.parent / f"{i:08d}.mp4").exists():
+            return
+        else:
+            M3U8 += f"""#EXTINF:15.000000,
+{i:08d}.mp4
+"""
 
-    next_file = directory / f"{next_tracker:08d}.mp4"
+    M3U8 += "#EXT-X-ENDLIST"
 
-    if not next_file.exists():
-        lock_file.unlink()
-        return False
+    with open(file_path.parent / f"{file_path.stem}_temp.m3u8", "w") as f:
+        f.write(M3U8)
 
-    # append the next file to out.mp4
-    out_file = directory / "out.mp4"
+    subprocess.run(
+        [
+            "ffmpeg",
+            "-i",
+            str(file_path.parent / f"{file_path.stem}_temp.m3u8"),
+            "-ss",
+            str(int(file_path.stem) * 15),
+            "-to",
+            str((int(file_path.stem) + 1) * 15),
+            "-c",
+            "copy",
+            str(transcode_output),
+        ],
+        check=True,
+    )
 
-    if not out_file.exists():
-        out_file.touch()
-
-    with open(out_file, "ab") as out:
-        with open(next_file, "rb") as next:
-            out.write(next.read())
-
-    # delete the next file
-    next_file.unlink()
-
-    # increment the next tracker
-    next_tracker += 1
-    next_tracker_file.write_text(str(next_tracker))
-
-    lock_file.unlink()
-    return True
-
-
-def concat_all(directory: pathlib.Path) -> None:
-    while concat(directory):
-        pass
+    (file_path.parent / f"{file_path.stem}_temp.m3u8").unlink()
